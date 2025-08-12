608fcdc8d7a62c8198e0d7cd3b6d6d3f7624a8df
DevTruce <DevTruce@gmail.com>
2025-08-12 18:04:08 -0400

    change api key

--- src/js/config.js
+++ src/js/config.js
@@ -1,7 +1,5 @@
-const apiKey = process.env.API_KEY;
-
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = apiKey;
+export const KEY = process.env.API_KEY;
 export const MODAL_CLOSE_SEC = 2.5;
